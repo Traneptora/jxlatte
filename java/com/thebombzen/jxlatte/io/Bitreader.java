d4915921dc45e2d418b7fd44fabaaed336b7e526
Leo Izen <leo.izen@gmail.com>
2023-07-16 22:45:31 -0400

    Bitreader: support subnormal floats

--- java/com/thebombzen/jxlatte/io/Bitreader.java
+++ java/com/thebombzen/jxlatte/io/Bitreader.java
@@ -58,13 +58,21 @@ public class Bitreader extends InputStream {
         int bits16 = readBits(16);
         int mantissa = bits16 & 0x3FF;
         int biased_exp = (bits16 >>> 10) & 0x1F;
-        // 143 == 31 + 127 - 15 == 31 + 112
         if (biased_exp == 31)
             throw new InvalidBitstreamException("Illegal infinite/NaN float16");
         biased_exp += 127 - 15;
         mantissa <<= 13;
         int sign = (bits16 & 0x8000) << 16;
-        int total = sign | (biased_exp << 23) | mantissa;
+        int total;
+        if (biased_exp == 112) {
+            if (mantissa == 0) {
+                biased_exp = 0;
+            } else {
+                int clz = Integer.numberOfLeadingZeros(mantissa);
+                mantissa = (mantissa << (clz - 8)) & 0x7FFFFF;
+            }
+        }
+        total = sign | (biased_exp << 23) | mantissa;
         return Float.intBitsToFloat(total);
     }
 
